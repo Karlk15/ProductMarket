2fd602d83107531c19e1a39e52372c29ea4b1c8c
Daníel Melstað <danielm15@ru.is>
2017-03-10 17:30:02 +0000

    Changes to service test class

--- client/src/app/sellers.service.spec.ts
+++ client/src/app/sellers.service.spec.ts
@@ -1,15 +1,65 @@
 /* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, getTestBed, async, inject } from '@angular/core/testing';
+import { Headers, BaseRequestOptions, Response, HttpModule, Http, XHRBackend, RequestMethod } from '@angular/http';
+import { ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 import { SellersService } from './sellers.service';
+import { Seller } from '../interfaces/seller';
+import { } from 'jasmine';
 
 describe('SellersService', () => {
-  beforeEach(() => {
+
+  let mockBackend: MockBackend;
+
+  beforeEach((async) => {
     TestBed.configureTestingModule({
-      providers: [SellersService]
+      providers: [
+        SellersService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          deps: [MockBackend, BaseRequestOptions],
+          useFactory:
+            (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+              return new Http(backend, defaultOptions);
+            }
+        }
+      ],
+      imports: [ HttpModule ]
     });
+    mockBackend = getTestBed().get(MockBackend);
   });
 
+
+  it('should get sellers', done => {
+    let sellersService: SellersService;
+
+    getTestBed().compileComponents().then() => {
+      mockBackend.connections.subscribe(
+        (connection: MockConnection) => {
+          connection.mockRespond(new Response(
+            new ResponseOptions({
+              body: [
+                {
+                  id: 2,
+                  name: 'Danni',
+                  category: 'Cats',
+                  imagePath: 'http://imgur.com/r/cats/xXYgX7h'
+                }]
+            }
+            )));
+        });
+
+        sellersService = getTestBed().get(SellersService);
+        expect(sellersService).toBeDefined();
+
+        sellersService.getSellers().subscribe(
+          (Seller) => {
+            expect(Seller.id).toBe(2);
+          })
+    }
+  })
   it('should ...', inject([SellersService], (service: SellersService) => {
     expect(service).toBeTruthy();
   }));
