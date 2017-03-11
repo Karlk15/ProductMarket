eb6320f45d4c14a35fa8e9cedbd5fe0f4d29dbc8
Daníel Melstað <danielm15@ru.is>
2017-03-11 17:38:45 +0000

    Testing for sellerService get functions

--- client/src/app/seller-dlg/seller-dlg.component.spec.ts
+++ client/src/app/seller-dlg/seller-dlg.component.spec.ts
@@ -2,7 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { SellerDlgComponent } from './seller-dlg.component';
 
 describe('SellerDlgComponent', () => {
